0250ee95b9e319ffec7078f9a5c296f79809d017
Andre Petersen <mail@andrepetersen.de>
2022-05-28 20:08:14 +0200

    Added note for myself regarding quali and race durations

---
+++ organizer/events/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+# make quali_duration and race_duration dependend on quali/race duration mode
+# https://docs.djangoproject.com/en/4.0/ref/forms/validation/#cleaning-and-validating-fields-that-depend-on-each-other
