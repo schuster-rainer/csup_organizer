4cfc76131f9b7a0a12b711cc1801bcd4a507cf14
Andre Petersen <mail@andrepetersen.de>
2022-05-29 12:27:39 +0200

    Added the models to admin page

--- organizer/content/admin.py
+++ organizer/content/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Track,Car
+
+admin.site.register(Track)
+admin.site.register(Car)
--- organizer/events/admin.py
+++ organizer/events/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import TeamLeague, SingleLeague, Race, RaceResults, Penalty
+
+admin.site.register(TeamLeague)
+admin.site.register(SingleLeague)
+admin.site.register(Race)
+admin.site.register(RaceResults)
+admin.site.register(Penalty)
