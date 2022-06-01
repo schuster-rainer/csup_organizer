72b53f6ad738be9697504575db132ac60ba946ad
Andre Petersen <mail@andrepetersen.de>
2022-06-01 15:58:09 +0200

    No request needed due to discord login only

--- organizer/organizer/views.py
+++ organizer/organizer/views.py
@@ -7,15 +7,3 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "index.html"
-
-def register_request(request):
-	if request.method == "POST":
-		form = RegistrationForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("main:homepage")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = RegistrationForm()
-	return render (request=request, template_name="registration/register.html", context={"register_form":form})
